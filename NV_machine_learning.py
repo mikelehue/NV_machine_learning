f7c1e3be70118b5b43a5f25070b6dcfb7339d33f
miguel <miguelvarga@gmail.com>
2025-05-20 11:02:45 +0200

    Creation of the script NV_machine_learning.

---
+++ NV_machine_learning.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Filename: NV_machine_learning.py
+Description: This script simulates a one qubit cluster sorter by means of a NV center on diamond.
+Authors: Asier Mongelos and Miguel Lopez Varga
+Creation date: 2025-05-20
+"""
